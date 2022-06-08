f37e894acb2c74a2b8d360917bd7723de4dcd2c5
MauricioHdz <mauricioisraelhernandez@gmail.com>
2022-06-08 14:22:18 -0500

    Se ajusto controlador y orm de org_puesto

--- controllers/controlador_org_puesto.php
+++ controllers/controlador_org_puesto.php
@@ -2,12 +2,13 @@
 
 namespace controllers;
 
-use config\generales;
-use gamboamartin\errores\errores;
-use JsonException;
-use stdClass;
+use models\org_puesto;
+use PDO;
+use base\controller\controlador_base;
 
-class controlador_org_puesto
-{
-
+class controlador_org_puesto  extends controlador_base{
+    public function __construct(PDO $link){
+        $modelo = new org_puesto(link: $link);
+        parent::__construct(link: $link,modelo:  $modelo);
+    }
 }
--- orm/org_puesto.php
+++ orm/org_puesto.php
@@ -1,10 +1,15 @@
 <?php
 namespace models;
 use base\orm\modelo;
-use gamboamartin\errores\errores;
 use PDO;
-use stdClass;
 
 class org_puesto extends modelo{
+    public function __construct(PDO $link){
+        $tabla = __CLASS__;
+        $columnas = array($tabla=>false);
+        $campos_obligatorios = array();
 
+        parent::__construct(link: $link,tabla:  $tabla, campos_obligatorios: $campos_obligatorios,
+            columnas: $columnas);
+    }
 }
