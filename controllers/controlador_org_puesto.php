36ef0e4003082a31a3ea44f1f3260b4423fe05bd
Angel Martínez De León <angelmartinezdeleon@gmail.com>
2022-06-09 14:48:27 -0500

    cambios controlador_org_puesto

--- controllers/controlador_org_puesto.php
+++ controllers/controlador_org_puesto.php
@@ -2,13 +2,29 @@
 
 namespace controllers;
 
+use config\generales;
+use gamboamartin\errores\errores;
+use html\html;
 use models\org_puesto;
 use PDO;
 use base\controller\controlador_base;
 
 class controlador_org_puesto  extends controlador_base{
+
+    public string $include_menu = '';
+    public string $link_inicio = '';
+    private html $html;
+
     public function __construct(PDO $link){
         $modelo = new org_puesto(link: $link);
         parent::__construct(link: $link,modelo:  $modelo);
+    }
+
+    public function alta(bool $header = true, bool $ws = false): string|array
+    {
+        $this->include_menu = (new generales())->path_base;
+        $this->include_menu .= 'templates/_formulario';
+
+        return $this->include_menu;
     }
 }
