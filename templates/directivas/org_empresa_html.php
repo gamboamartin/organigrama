5150fbd9736815b63389ed96ae071f70e9abc442
mgamboa <gamboa.martin@gmail.com>
2022-06-29 13:24:18 -0500

    v0.3.0 Se genera paquete

--- templates/directivas/org_empresa_html.php
+++ templates/directivas/org_empresa_html.php
@@ -9,10 +9,11 @@ use PDO;
 
 class org_empresa_html extends html_controler {
 
-    public function select_cat_sat_regimen_fiscal_id(PDO $link): array|string
+    public function select_cat_sat_regimen_fiscal_id(int $id_selected, PDO $link): array|string
     {
         $modelo = new cat_sat_regimen_fiscal($link);
-        $select = $this->select_catalogo(modelo: $modelo);
+
+        $select = $this->select_catalogo(id_selected:$id_selected, modelo: $modelo);
         if(errores::$error){
             return $this->error->error(mensaje: 'Error al generar select', data: $select);
         }
