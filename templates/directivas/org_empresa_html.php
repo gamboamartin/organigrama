52d4f0082be0f551383469ab9b0ec9b54b615b57
mgamboa <gamboa.martin@gmail.com>
2022-06-30 12:31:35 -0500

    v0.5.0 Se henea input empresa

--- templates/directivas/org_empresa_html.php
+++ templates/directivas/org_empresa_html.php
@@ -1,9 +1,22 @@
 <?php
 namespace html;
 
+use gamboamartin\errores\errores;
 use gamboamartin\system\html_controler;
+use models\org_empresa;
+use PDO;
 
 class org_empresa_html extends html_controler {
 
+    public function select_org_empresa_id(int $id_selected, PDO $link): array|string
+    {
+        $modelo = new org_empresa($link);
+
+        $select = $this->select_catalogo(id_selected:$id_selected, modelo: $modelo);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al generar select', data: $select);
+        }
+        return $select;
+    }
 
 }
