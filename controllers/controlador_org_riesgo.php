19b22921dfd6b38cddcd0c67d570aaf287e05e42
Angel Martínez De León <angelmartinezdeleon@gmail.com>
2022-07-04 14:16:50 -0500

    Se agregó entidad org_riesgo

---
+++ controllers/controlador_org_riesgo.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @author Martin Gamboa Vazquez
+ * @version 1.0.0
+ * @created 2022-05-14
+ * @final En proceso
+ *
+ */
+namespace controllers;
+
+use gamboamartin\system\links_menu;
+use gamboamartin\system\system;
+use html\org_riesgo_html;
+use models\org_riesgo;
+use PDO;
+use stdClass;
+
+class controlador_org_riesgo extends system {
+
+    public function __construct(PDO $link, stdClass $paths_conf = new stdClass()){
+        $modelo = new org_riesgo(link: $link);
+        $html = new org_riesgo_html();
+        $obj_link = new links_menu($this->registro_id);
+        parent::__construct(html:$html, link: $link,modelo:  $modelo, obj_link: $obj_link, paths_conf: $paths_conf);
+
+        $this->titulo_lista = 'Riesgos';
+
+    }
+
+
+
+
+}
---
+++ templates/directivas/org_riesgo_html.php
@@ -0,0 +1,10 @@
+<?php
+namespace html;
+
+use gamboamartin\system\html_controler;
+
+
+class org_riesgo_html extends html_controler {
+
+
+}
