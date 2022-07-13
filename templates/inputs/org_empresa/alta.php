ca6c5fdd5f70ad45da7cf2f40b218d6346ada5f3
martingamboaivitec <martin.gamboa@ivitec.com.mx>
2022-07-13 12:49:50 -0500

    v0.9.0 Se integra btn accion alta pais en empresa

--- templates/inputs/org_empresa/alta.php
+++ templates/inputs/org_empresa/alta.php
@@ -6,6 +6,12 @@
 <?php echo $controlador->inputs->nombre_comercial; ?>
 <?php echo $controlador->inputs->select->cat_sat_regimen_fiscal_id; ?>
 <?php echo $controlador->inputs->select->dp_pais_id; ?>
-
-
 <?php include (new views())->ruta_templates.'botons/submit/alta_bd_otro.php';?>
+
+<div class="control-group btn-alta">
+    <div class="controls"><?php include 'templates/botons/cat_sat_regimen_fiscal_alta.php';?></div>
+</div>
+
+<div class="control-group btn-alta">
+    <div class="controls"><?php include 'templates/botons/dp_pais_alta.php';?></div>
+</div>
