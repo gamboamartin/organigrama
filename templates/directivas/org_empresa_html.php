06ddec0e93523b19530531bd8cec170676d35ad0
martingamboaivitec <martin.gamboa@ivitec.com.mx>
2022-07-15 11:39:08 -0500

    v0.27.0 Se integra email sat a backend empresa

--- templates/directivas/org_empresa_html.php
+++ templates/directivas/org_empresa_html.php
@@ -38,6 +38,29 @@ class org_empresa_html extends html_controler {
         return $controler->inputs;
     }
 
+    public function em_email_sat(int $cols, stdClass $row_upd, bool $value_vacio): array|string
+    {
+        if($cols<=0){
+            return $this->error->error(mensaje: 'Error cold debe ser mayor a 0', data: $cols);
+        }
+        if($cols>=13){
+            return $this->error->error(mensaje: 'Error cold debe ser menor o igual a  12', data: $cols);
+        }
+
+        $html =$this->directivas->email_required(disable: false,name: 'fecha_inicio_operaciones',
+            place_holder: 'Inicio de Operaciones',row_upd: $row_upd, value_vacio: $value_vacio);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al generar input', data: $html);
+        }
+
+        $div = $this->directivas->html->div_group(cols: $cols,html:  $html);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al integrar div', data: $div);
+        }
+
+        return $div;
+    }
+
     public function fec_fecha_inicio_operaciones(int $cols, stdClass $row_upd, bool $value_vacio): array|string
     {
 
