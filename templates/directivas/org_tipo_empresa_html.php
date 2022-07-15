2ed98d6e98ad95e915c2e3bad38011854349a9b8
Keibei <kevin.acuna@ivitec.com.mx>
2022-07-15 14:07:50 -0500

    v0.32.0 Se genero CRUD para la entidad org_tipo_empresa

---
+++ templates/directivas/org_tipo_empresa_html.php
@@ -0,0 +1,14 @@
+<?php
+namespace html;
+
+use gamboamartin\errores\errores;
+use gamboamartin\system\html_controler;
+use models\org_tipo_actividad;
+use PDO;
+use stdClass;
+
+
+class org_tipo_empresa_html extends html_controler {
+
+
+}
