c0ef040779c8a0ab837ca50055e141dfd054a6c9
Keibei <kevin.acuna@ivitec.com.mx>
2022-07-15 15:31:49 -0500

    v0.33.0 Se genero CRUD para la entidad org_empresa_clasificada

--- templates/directivas/org_tipo_empresa_html.php
+++ templates/directivas/org_tipo_empresa_html.php
@@ -4,11 +4,24 @@ namespace html;
 use gamboamartin\errores\errores;
 use gamboamartin\system\html_controler;
 use models\org_tipo_actividad;
+use models\org_tipo_empresa;
 use PDO;
 use stdClass;
 
 
 class org_tipo_empresa_html extends html_controler {
+
+    public function select_org_tipo_empresa_id(int $cols,bool $con_registros,int $id_selected, PDO $link): array|string
+    {
+        $modelo = new org_tipo_empresa($link);
+
+        $select = $this->select_catalogo(cols:$cols,con_registros:$con_registros,id_selected:$id_selected,
+            modelo: $modelo);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al generar select', data: $select);
+        }
+        return $select;
+    }
 
 
 }
