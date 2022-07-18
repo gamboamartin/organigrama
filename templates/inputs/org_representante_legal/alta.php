180e00bccbc3599a8b0b969171dced4ecb3c20e8
Keibei <kevin.acuna@ivitec.com.mx>
2022-07-18 11:01:07 -0500

    v0.38.1 Se ajusta la vista de org_porcentaje_act_economica agregando campos faltantes

--- templates/inputs/org_representante_legal/alta.php
+++ templates/inputs/org_representante_legal/alta.php
@@ -1,8 +1,5 @@
 <?php /** @var controllers\controlador_org_empresa $controlador  controlador en ejecucion */ ?>
 <?php use config\views; ?>
-<?php echo $controlador->inputs->codigo; ?>
-<?php echo $controlador->inputs->codigo_bis; ?>
-<?php echo $controlador->inputs->descripcion; ?>
 <?php echo $controlador->inputs->nombre; ?>
 <?php echo $controlador->inputs->rfc; ?>
 <?php echo $controlador->inputs->a_paterno; ?>
