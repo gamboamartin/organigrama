9987c2e18dc759afa09edde5796984982cd9c7ae
Keibei <kevin.acuna@ivitec.com.mx>
2022-07-27 09:29:46 -0500

    v.69.15 Fix parametros html_base org_empresa_clasificada

--- controllers/controlador_org_empresa_clasificada.php
+++ controllers/controlador_org_empresa_clasificada.php
@@ -11,6 +11,7 @@ namespace gamboamartin\organigrama\controllers;
 use gamboamartin\errores\errores;
 use gamboamartin\system\links_menu;
 use gamboamartin\system\system;
+use gamboamartin\template\html;
 use html\org_empresa_clasificada_html;
 use html\org_empresa_html;
 use html\org_tipo_empresa_html;
@@ -21,9 +22,10 @@ use stdClass;
 
 class controlador_org_empresa_clasificada extends system {
 
-    public function __construct(PDO $link, stdClass $paths_conf = new stdClass()){
+    public function __construct(PDO $link, html $html = new \gamboamartin\template_1\html(),
+                                stdClass $paths_conf = new stdClass()){
         $modelo = new org_empresa_clasificada(link: $link);
-        $html = new org_empresa_clasificada_html();
+        $html = new org_empresa_clasificada_html(html: $html);
         $obj_link = new links_menu($this->registro_id);
         parent::__construct(html:$html, link: $link,modelo:  $modelo, obj_link: $obj_link, paths_conf: $paths_conf);
 
@@ -39,7 +41,7 @@ class controlador_org_empresa_clasificada extends system {
 
         $this->inputs->select = new stdClass();
 
-        $select = (new org_empresa_html())->select_org_empresa_id(cols:12,con_registros:true,id_selected:-1,link: $this->link);
+        $select = (new org_empresa_html($this->html_base))->select_org_empresa_id(cols:12,con_registros:true,id_selected:-1,link: $this->link);
         if(errores::$error){
             $error = $this->errores->error(mensaje: 'Error al generar select',data:  $select);
             print_r($error);
@@ -47,7 +49,7 @@ class controlador_org_empresa_clasificada extends system {
         }
         $this->inputs->select->org_empresa_id = $select;
 
-        $select = (new org_tipo_empresa_html())->select_org_tipo_empresa_id(cols:12,con_registros:true,id_selected:-1,link: $this->link);
+        $select = (new org_tipo_empresa_html(html: $this->html_base))->select_org_tipo_empresa_id(cols:12,con_registros:true,id_selected:-1,link: $this->link);
         if(errores::$error){
             $error = $this->errores->error(mensaje: 'Error al generar select',data:  $select);
             print_r($error);
