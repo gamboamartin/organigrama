893a2d8e2bcaee6ee9f4ce12f8ee6d5acf840141
Keibei <kevin.acuna@ivitec.com.mx>
2022-07-28 12:42:33 -0500

    v.76.17 Se agrega campo tipo empresa a modifica

--- templates/directivas/org_empresa_html.php
+++ templates/directivas/org_empresa_html.php
@@ -708,6 +708,15 @@ class org_empresa_html extends html_controler {
 
         $selects->dp_calle_pertenece_entre2_id = $select;
 
+        $select = (new org_tipo_empresa_html(html: $this->html_base))->select_org_tipo_empresa_id(
+            cols: 12, con_registros:false, id_selected:-1,link: $link);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al generar select',data:  $select);
+
+        }
+
+        $selects->org_tipo_empresa_id = $select;
+
         return $selects;
     }
 
