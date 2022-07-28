b2275e03d08a897fc87b63e3098ea31b3e2b35b3
Keibei <kevin.acuna@ivitec.com.mx>
2022-07-28 10:59:19 -0500

    v.75.17 Fix se ajusta campo html_base en tipo empresa

--- controllers/controlador_org_tipo_empresa.php
+++ controllers/controlador_org_tipo_empresa.php
@@ -11,6 +11,7 @@ namespace gamboamartin\organigrama\controllers;
 use gamboamartin\errores\errores;
 use gamboamartin\system\links_menu;
 use gamboamartin\system\system;
+use gamboamartin\template\html;
 use html\org_tipo_empresa_html;
 use models\org_tipo_empresa;
 use PDO;
@@ -18,9 +19,10 @@ use stdClass;
 
 class controlador_org_tipo_empresa extends system {
 
-    public function __construct(PDO $link, stdClass $paths_conf = new stdClass()){
+    public function __construct(PDO $link, html $html = new \gamboamartin\template_1\html(),
+                                stdClass $paths_conf = new stdClass()){
         $modelo = new org_tipo_empresa(link: $link);
-        $html = new org_tipo_empresa_html();
+        $html = new org_tipo_empresa_html(html: $html);
         $obj_link = new links_menu($this->registro_id);
         parent::__construct(html:$html, link: $link,modelo:  $modelo, obj_link: $obj_link, paths_conf: $paths_conf);
 
