8894f23408921af02e8d5b57ed9ab3d4f24a979a
martingamboaivitec <martin.gamboa@ivitec.com.mx>
2022-08-02 17:54:20 -0500

    v0.96.26 Se simpplifica entrada de java

--- js/org_sucursal/alta.js
+++ js/org_sucursal/alta.js
@@ -1,18 +1,14 @@
-function getParameterByName(name) {
-    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-    const regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-        results = regex.exec(location.search);
-    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+function getAbsolutePath() {
+    var loc = window.location;
+    var pathName = loc.pathname.substring(0, loc.pathname.lastIndexOf('/') + 1);
+    return loc.href.substring(0, loc.href.length - ((loc.pathname + loc.search + loc.hash).length - pathName.length));
 }
-function integra_new_option(container, descripcion, value){
-    let new_option =new_option_sl(descripcion,value);
-    $(new_option).appendTo(container);
-}
+let url = getAbsolutePath();
+let base_js = url+'js/base.js';
+document.write('<script src="'+base_js+'"></script>');
 
 
-function new_option_sl(descripcion,value){
-    return "<option value =" + value + ">" + descripcion + "</option>";
-}
+
 let session_id = getParameterByName('session_id');
 let dp_pais_id = -1;
 let fecha_inicio_operaciones = '0000-00-00';
