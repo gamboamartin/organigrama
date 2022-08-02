f8ba56d0f319435ccf3e8d6f349104690bf32b98
martingamboaivitec <martin.gamboa@ivitec.com.mx>
2022-08-02 17:11:42 -0500

    v0.94.26 Se integra pais id en evento de seleccion de empresa

--- js/org_sucursal/alta.js
+++ js/org_sucursal/alta.js
@@ -15,20 +15,27 @@ function new_option_sl(descripcion,value){
 }
 let session_id = getParameterByName('session_id');
 let dp_pais_id = -1;
-let dp_estado_id = -1;
-let dp_municipio_id = -1;
-let dp_cp_id = -1;
-let dp_colonia_postal_id = -1;
+let fecha_inicio_operaciones = '0000-00-00';
 
+
+let sl_dp_pais_id = $("#dp_pais_id");
 let sl_org_empresa_id = $("#org_empresa_id");
+
 let txt_fecha_inicio_operaciones = $('#fecha_inicio_operaciones');
 
+
 sl_org_empresa_id.change(function(){
+
     let selected = $(this).find('option:selected');
-    let fecha_inicio_operaciones = selected.data('org_empresa_fecha_inicio_operaciones');
+
+    fecha_inicio_operaciones = selected.data('org_empresa_fecha_inicio_operaciones');
+    dp_pais_id = selected.data('dp_pais_id');
+
     if(fecha_inicio_operaciones !== '0000-00-00'){
         txt_fecha_inicio_operaciones.val(fecha_inicio_operaciones);
     }
+    sl_dp_pais_id.val(dp_pais_id);
+    sl_dp_pais_id.selectpicker('refresh')
 
 });
 
