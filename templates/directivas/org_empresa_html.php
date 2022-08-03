eb425d33bde90adaaf8a8576c056e342ef1cc8ba
martingamboaivitec <martin.gamboa@ivitec.com.mx>
2022-08-03 15:40:45 -0500

    0.111.26 Se integra filtro para cp

--- templates/directivas/org_empresa_html.php
+++ templates/directivas/org_empresa_html.php
@@ -563,10 +563,13 @@ class org_empresa_html extends html_controler {
 
 
         $selects->dp_municipio_id = $select;
+        $filtro = array();
+        if($dp_municipio_id!==-1){
+            $filtro['dp_municipio.id'] = $dp_municipio_id;
+        }
 
-
-        $select = (new dp_cp_html(html: $this->html_base))->select_dp_cp_id(cols: 6, con_registros:false,
-            id_selected:-1,link: $link);
+        $select = (new dp_cp_html(html: $this->html_base))->select_dp_cp_id(cols: 6, con_registros:true,
+            id_selected:-1,link: $link, filtro: $filtro);
         if(errores::$error){
             return $this->error->error(mensaje: 'Error al generar select',data:  $select);
 
