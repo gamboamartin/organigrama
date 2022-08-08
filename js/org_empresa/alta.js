82bd4d0854f96a7ecc5c9dfc3271882f19f2e3f1
martingamboaivitec <martin.gamboa@ivitec.com.mx>
2022-08-08 16:29:54 -0500

    0.141.29 Se implementa base de direcciones en empresa

--- js/org_empresa/alta.js
+++ js/org_empresa/alta.js
@@ -1,14 +1,8 @@
-function getAbsolutePath() {
-    var loc = window.location;
-    var pathName = loc.pathname.substring(0, loc.pathname.lastIndexOf('/') + 1);
-    return loc.href.substring(0, loc.href.length - ((loc.pathname + loc.search + loc.hash).length - pathName.length));
-}
 let url = getAbsolutePath();
 
-let direcciones_js = url+'js/direcciones.js';
+let direcciones_js = url+'vendor/gamboa.martin/js_base/src/direcciones.js';
 
 document.write('<script src="'+direcciones_js+'"></script>');
-
 
 
 let session_id = getParameterByName('session_id');
--- js/org_empresa/modifica.js
+++ js/org_empresa/modifica.js
@@ -1,20 +1,10 @@
-function getAbsolutePath() {
-    var loc = window.location;
-    var pathName = loc.pathname.substring(0, loc.pathname.lastIndexOf('/') + 1);
-    return loc.href.substring(0, loc.href.length - ((loc.pathname + loc.search + loc.hash).length - pathName.length));
-}
 let url = getAbsolutePath();
-
-let direcciones_js = url+'js/direcciones.js';
-
+let direcciones_js = url+'vendor/gamboa.martin/js_base/src/direcciones.js';
 document.write('<script src="'+direcciones_js+'"></script>');
-
-
-
 let session_id = getParameterByName('session_id');
 
-let sl_local_dp_colonia_postal_id = $("#dp_colonia_postal_id");
 
+let sl_local_dp_colonia_postal_id = $("#dp_colonia_postal_id");
 
 let sl_dp_calle_pertenece_entre1_id = $("#dp_calle_pertenece_entre1_id");
 let sl_dp_calle_pertenece_entre2_id = $("#dp_calle_pertenece_entre2_id");
