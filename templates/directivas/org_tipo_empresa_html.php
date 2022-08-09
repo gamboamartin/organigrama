8f91d14e516654329de92c1f22eaf4b30b49ab77
martingamboaivitec <martin.gamboa@ivitec.com.mx>
2022-08-09 14:34:27 -0500

    v.0.146.30 Se integra tipo sucursal

--- templates/directivas/org_tipo_empresa_html.php
+++ templates/directivas/org_tipo_empresa_html.php
@@ -3,10 +3,8 @@ namespace html;
 
 use gamboamartin\errores\errores;
 use gamboamartin\system\html_controler;
-use models\org_tipo_actividad;
 use models\org_tipo_empresa;
 use PDO;
-use stdClass;
 
 
 class org_tipo_empresa_html extends html_controler {
---
+++ templates/directivas/org_tipo_sucursal_html.php
@@ -0,0 +1,13 @@
+<?php
+namespace html;
+
+
+use gamboamartin\system\html_controler;
+
+
+
+class org_tipo_sucursal_html extends html_controler {
+
+
+
+}
