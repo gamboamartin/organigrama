5eb6857e6008d10b8ef9e22b6e6fd21773871de1
martingamboaivitec <martin.gamboa@ivitec.com.mx>
2022-08-09 13:49:51 -0500

    v.0.143.30 Se integra sucursal con domicilios

--- js/org_sucursal/alta.js
+++ js/org_sucursal/alta.js
@@ -1,19 +1,11 @@
-function getAbsolutePath() {
-    var loc = window.location;
-    var pathName = loc.pathname.substring(0, loc.pathname.lastIndexOf('/') + 1);
-    return loc.href.substring(0, loc.href.length - ((loc.pathname + loc.search + loc.hash).length - pathName.length));
-}
 let url = getAbsolutePath();
-let base_js = url+'js/base.js';
-let direcciones_js = url+'js/direcciones.js';
-document.write('<script src="'+base_js+'"></script>');
+
+let direcciones_js = url+'vendor/gamboa.martin/js_base/src/direcciones.js';
+
 document.write('<script src="'+direcciones_js+'"></script>');
 
 
-
 let session_id = getParameterByName('session_id');
-
-let fecha_inicio_operaciones = '0000-00-00';
 
 
 let sl_org_empresa_id = $("#org_empresa_id");
