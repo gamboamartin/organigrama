da398cfde37e7a4e533b36cefa3cff788e652301
MauricioHdz <mauricioisraelhernandez@gmail.com>
2022-08-30 11:38:55 -0500

    v0.274.35 Se integra get_sucursales

---
+++ js/org_empresa/registros_patronales.js
@@ -0,0 +1,34 @@
+let url = getAbsolutePath();
+let direcciones_js = url+'vendor/gamboa.martin/js_base/src/direcciones.js';
+document.write('<script src="'+direcciones_js+'"></script>');
+let session_id = getParameterByName('session_id');
+
+
+$( document ).ready(function() {
+    let sl_org_empresa_id = $("#org_empresa_id");
+    let sl_org_sucursal_id = $("#org_sucursal_id");
+
+    let org_empresa_id = sl_org_empresa_id.val();
+    let url = "index.php?seccion=org_sucursal&ws=1&accion=get_sucursal&org_empresa_id="+org_empresa_id+"&session_id="+session_id;
+
+    $.ajax({
+        type: 'GET',
+        url: url,
+    }).done(function( data ) {  // Función que se ejecuta si todo ha ido bien
+        sl_org_sucursal_id.empty();
+
+        integra_new_option("#org_sucursal_id",'Seleccione una calle','-1');
+
+        $.each(data.registros, function( index, org_sucursal ) {
+            integra_new_option("#org_sucursal_id",org_sucursal.org_tipo_sucursal_descripcion+
+                ' - '+ org_sucursal.dp_colonia_descripcion+ ' '+org_sucursal.dp_calle_descripcion+
+                ' '+org_sucursal.org_sucursal_exterior+ ' '+org_sucursal.org_sucursal_interior,
+                org_sucursal.org_sucursal_id);
+        });
+        sl_org_sucursal_id.selectpicker('refresh');
+    }).fail(function (jqXHR, textStatus, errorThrown){ // Función que se ejecuta si algo ha ido mal
+        alert('Error al ejecutar');
+        console.log(url);
+    });
+
+});
