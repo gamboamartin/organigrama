30e2cc02568be4044aa713e8ce14bec25d3505d8
mgamboa <gamboa.martin@gmail.com>
2022-09-21 17:40:26 -0500

    v0.288.38 FIX depto in puesto

--- orm/org_puesto.php
+++ orm/org_puesto.php
@@ -6,7 +6,8 @@ use PDO;
 class org_puesto extends modelo{
     public function __construct(PDO $link){
         $tabla = __CLASS__;
-        $columnas = array($tabla=>false,'org_tipo_puesto'=>$tabla,'org_departamento'=>$tabla,'org_empresa'=>'departamento');
+        $columnas = array($tabla=>false,'org_tipo_puesto'=>$tabla,'org_departamento'=>$tabla,
+            'org_empresa'=>'org_departamento');
         $campos_obligatorios = array('org_tipo_puesto_id','org_departamento_id');
 
         parent::__construct(link: $link,tabla:  $tabla, campos_obligatorios: $campos_obligatorios,
