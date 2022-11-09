da3d8d679fe98802cbb5f9ac40f7809a242582b4
Kevin <kevin.acuna@ivitec.com.mx>
2022-11-09 14:27:27 -0500

    v0.349.42 Fix se ajustan inputs dp

--- templates/inputs/dp_estado/alta.php
+++ templates/inputs/dp_estado/alta.php
@@ -1,7 +1,7 @@
 <?php /** @var controllers\controlador_dp_estado $controlador  controlador en ejecucion */ ?>
 <?php use config\views; ?>
 <?php echo $controlador->forms_inputs_alta; ?>
-<?php echo $controlador->inputs->select->dp_pais_id; ?>
+<?php echo $controlador->inputs->dp_pais_id; ?>
 <?php include (new views())->ruta_templates.'botons/submit/alta_bd_otro.php';?>
 <div class="control-group btn-alta">
     <div class="controls"><?php include 'templates/botons/dp_pais_alta.php';?></div>
