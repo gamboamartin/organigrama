0da236e2c09a6a861d3407cf840b0e25f911d423
Kevin <kevin.acuna@ivitec.com.mx>
2022-11-16 17:54:49 -0500

    v0.352.42 Se ajusta tipo sucursal

--- controllers/controlador_org_tipo_sucursal.php
+++ controllers/controlador_org_tipo_sucursal.php
@@ -8,6 +8,7 @@
  */
 namespace gamboamartin\organigrama\controllers;
 
+use gamboamartin\errores\errores;
 use gamboamartin\organigrama\models\org_tipo_sucursal;
 use gamboamartin\system\links_menu;
 use gamboamartin\system\system;
@@ -18,15 +19,92 @@ use stdClass;
 
 class controlador_org_tipo_sucursal extends system {
 
+    public array $keys_selects = array();
+
     public function __construct(PDO $link, html $html = new \gamboamartin\template_1\html(),
                                 stdClass $paths_conf = new stdClass()){
         $modelo = new org_tipo_sucursal(link: $link);
-        $html_ = new org_tipo_sucursal_html(html: $html);
+        $html = new org_tipo_sucursal_html(html: $html);
         $obj_link = new links_menu(link: $link, registro_id:$this->registro_id);
-        parent::__construct(html:$html_, link: $link,modelo:  $modelo, obj_link: $obj_link, paths_conf: $paths_conf);
+
+        $columns["org_tipo_sucursal_id"]["titulo"] = "Id";
+        $columns["org_tipo_sucursal_codigo"]["titulo"] = "Código";
+        $columns["org_tipo_sucursal_descripcion"]["titulo"] = "Tipo Sucursal";
+
+        $filtro = array("org_tipo_sucursal.id","org_tipo_sucursal.codigo","org_tipo_sucursal.descripcion");
+
+        $datatables = new stdClass();
+        $datatables->columns = $columns;
+        $datatables->filtro = $filtro;
+
+        parent::__construct(html:$html, link: $link,modelo:  $modelo, obj_link: $obj_link, datatables: $datatables,
+            paths_conf: $paths_conf);
 
         $this->titulo_lista = 'Tipo Sucursal';
 
+        $propiedades = $this->inicializa_propiedades();
+        if(errores::$error){
+            $error = $this->errores->error(mensaje: 'Error al inicializar propiedades',data:  $propiedades);
+            print_r($error);
+            die('Error');
+        }
     }
 
+    public function alta(bool $header, bool $ws = false): array|string
+    {
+        $r_alta =  parent::alta(header: false);
+        if(errores::$error){
+            return $this->retorno_error(mensaje: 'Error al generar template',data:  $r_alta, header: $header,ws:$ws);
+        }
+
+        $inputs = $this->genera_inputs(keys_selects:  $this->keys_selects);
+        if(errores::$error){
+            $error = $this->errores->error(mensaje: 'Error al generar inputs',data:  $inputs);
+            print_r($error);
+            die('Error');
+        }
+
+        return $r_alta;
+    }
+
+    public function asignar_propiedad(string $identificador, mixed $propiedades)
+    {
+        if (!array_key_exists($identificador,$this->keys_selects)){
+            $this->keys_selects[$identificador] = new stdClass();
+        }
+
+        foreach ($propiedades as $key => $value){
+            $this->keys_selects[$identificador]->$key = $value;
+        }
+    }
+
+    private function inicializa_propiedades(): array
+    {
+        $identificador = "codigo";
+        $propiedades = array("place_holder" => "Código", "cols" => 4);
+        $this->asignar_propiedad(identificador:$identificador, propiedades: $propiedades);
+
+        $identificador = "descripcion";
+        $propiedades = array("place_holder" => "Tipo Sucursal", "cols" => 8);
+        $this->asignar_propiedad(identificador:$identificador, propiedades: $propiedades);
+
+        return $this->keys_selects;
+    }
+
+    public function modifica(bool $header, bool $ws = false): array|stdClass
+    {
+        $r_modifica =  parent::modifica(header: false);
+        if(errores::$error){
+            return $this->retorno_error(mensaje: 'Error al generar template',data:  $r_modifica, header: $header,ws:$ws);
+        }
+
+        $inputs = $this->genera_inputs(keys_selects:  $this->keys_selects);
+        if(errores::$error){
+            $error = $this->errores->error(mensaje: 'Error al generar inputs',data:  $inputs);
+            print_r($error);
+            die('Error');
+        }
+
+        return $r_modifica;
+    }
 }
--- orm/org_tipo_sucursal.php
+++ orm/org_tipo_sucursal.php
@@ -1,7 +1,9 @@
 <?php
 namespace gamboamartin\organigrama\models;
 use base\orm\modelo;
+use gamboamartin\errores\errores;
 use PDO;
+use stdClass;
 
 class org_tipo_sucursal extends modelo{
     public function __construct(PDO $link){
@@ -9,7 +11,59 @@ class org_tipo_sucursal extends modelo{
         $columnas = array($tabla=>false);
         $campos_obligatorios = array();
 
+        $campos_view['codigo'] = array('type' => 'inputs');
+        $campos_view['descripcion'] = array('type' => 'inputs');
+
         parent::__construct(link: $link,tabla:  $tabla, campos_obligatorios: $campos_obligatorios,
-            columnas: $columnas);
+            columnas: $columnas, campos_view: $campos_view);
+
+        $this->NAMESPACE = __NAMESPACE__;
+    }
+
+    public function alta_bd(): array|stdClass
+    {
+        $this->registro = $this->campos_base(data:$this->registro);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al inicializar campo base',data: $this->registro);
+        }
+
+        $r_alta_bd =  parent::alta_bd();
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al insertar unidad', data: $r_alta_bd);
+        }
+        return $r_alta_bd;
+    }
+
+    private function campos_base(array $data): array
+    {
+        if(!isset($data['codigo_bis'])){
+            $data['codigo_bis'] =  $data['codigo'];
+        }
+
+        if(!isset($data['descripcion_select'])){
+            $ds = str_replace("_"," ",$data['descripcion']);
+            $ds = ucwords($ds);
+            $data['descripcion_select'] =  "{$data['codigo']} - {$ds}";
+        }
+
+        if(!isset($data['alias'])){
+            $data['alias'] = $data['codigo'];
+        }
+        return $data;
+    }
+
+    public function modifica_bd(array $registro, int $id, bool $reactiva = false): array|stdClass
+    {
+        $registro = $this->campos_base(data:$registro);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al inicializar campo base',data: $registro);
+        }
+
+        $r_modifica_bd = parent::modifica_bd($registro, $id, $reactiva);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al modificar unidad',data:  $r_modifica_bd);
+        }
+
+        return $r_modifica_bd;
     }
 }
