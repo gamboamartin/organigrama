3362972aeee64df6950323e2eff71d1ff9bcbe9e
Kevin <kevin.acuna@ivitec.com.mx>
2022-11-16 12:01:33 -0500

    v0.351.42 Se ajusta tipo empresa

--- controllers/controlador_org_tipo_empresa.php
+++ controllers/controlador_org_tipo_empresa.php
@@ -8,6 +8,7 @@
  */
 namespace gamboamartin\organigrama\controllers;
 
+use gamboamartin\errores\errores;
 use gamboamartin\organigrama\models\org_tipo_empresa;
 use gamboamartin\system\links_menu;
 use gamboamartin\system\system;
@@ -18,15 +19,92 @@ use stdClass;
 
 class controlador_org_tipo_empresa extends system {
 
+    public array $keys_selects = array();
+
     public function __construct(PDO $link, html $html = new \gamboamartin\template_1\html(),
                                 stdClass $paths_conf = new stdClass()){
         $modelo = new org_tipo_empresa(link: $link);
         $html = new org_tipo_empresa_html(html: $html);
         $obj_link = new links_menu(link: $link, registro_id:$this->registro_id);
-        parent::__construct(html:$html, link: $link,modelo:  $modelo, obj_link: $obj_link, paths_conf: $paths_conf);
+
+        $columns["org_tipo_empresa_id"]["titulo"] = "Id";
+        $columns["org_tipo_empresa_codigo"]["titulo"] = "Código";
+        $columns["org_tipo_empresa_descripcion"]["titulo"] = "Tipo Empresa";
+
+        $filtro = array("org_tipo_empresa.id","org_tipo_empresa.codigo","org_tipo_empresa.descripcion");
+
+        $datatables = new stdClass();
+        $datatables->columns = $columns;
+        $datatables->filtro = $filtro;
+
+        parent::__construct(html:$html, link: $link,modelo:  $modelo, obj_link: $obj_link, datatables: $datatables,
+            paths_conf: $paths_conf);
 
         $this->titulo_lista = 'Tipo EMpresa';
 
+        $propiedades = $this->inicializa_propiedades();
+        if(errores::$error){
+            $error = $this->errores->error(mensaje: 'Error al inicializar propiedades',data:  $propiedades);
+            print_r($error);
+            die('Error');
+        }
     }
 
+    public function alta(bool $header, bool $ws = false): array|string
+    {
+        $r_alta =  parent::alta(header: false);
+        if(errores::$error){
+            return $this->retorno_error(mensaje: 'Error al generar template',data:  $r_alta, header: $header,ws:$ws);
+        }
+
+        $inputs = $this->genera_inputs(keys_selects:  $this->keys_selects);
+        if(errores::$error){
+            $error = $this->errores->error(mensaje: 'Error al generar inputs',data:  $inputs);
+            print_r($error);
+            die('Error');
+        }
+
+        return $r_alta;
+    }
+
+    public function asignar_propiedad(string $identificador, mixed $propiedades)
+    {
+        if (!array_key_exists($identificador,$this->keys_selects)){
+            $this->keys_selects[$identificador] = new stdClass();
+        }
+
+        foreach ($propiedades as $key => $value){
+            $this->keys_selects[$identificador]->$key = $value;
+        }
+    }
+
+    private function inicializa_propiedades(): array
+    {
+        $identificador = "codigo";
+        $propiedades = array("place_holder" => "Código", "cols" => 4);
+        $this->asignar_propiedad(identificador:$identificador, propiedades: $propiedades);
+
+        $identificador = "descripcion";
+        $propiedades = array("place_holder" => "Tipo Empresa", "cols" => 8);
+        $this->asignar_propiedad(identificador:$identificador, propiedades: $propiedades);
+
+        return $this->keys_selects;
+    }
+
+    public function modifica(bool $header, bool $ws = false): array|stdClass
+    {
+        $r_modifica =  parent::modifica(header: false);
+        if(errores::$error){
+            return $this->retorno_error(mensaje: 'Error al generar template',data:  $r_modifica, header: $header,ws:$ws);
+        }
+
+        $inputs = $this->genera_inputs(keys_selects:  $this->keys_selects);
+        if(errores::$error){
+            $error = $this->errores->error(mensaje: 'Error al generar inputs',data:  $inputs);
+            print_r($error);
+            die('Error');
+        }
+
+        return $r_modifica;
+    }
 }
