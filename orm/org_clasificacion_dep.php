6742cc6f72fa322def92831dbd48b00421eea6fe
mgamboa <gamboa.martin@gmail.com>
2022-12-05 17:06:58 -0600

    v0.367.48 Se integra n_deptos dentro de clasificacion

--- orm/org_clasificacion_dep.php
+++ orm/org_clasificacion_dep.php
@@ -1,9 +1,8 @@
 <?php
 namespace gamboamartin\organigrama\models;
 use base\orm\_modelo_parent_sin_codigo;
-use gamboamartin\errores\errores;
 use PDO;
-use stdClass;
+
 
 class org_clasificacion_dep extends _modelo_parent_sin_codigo{
     public function __construct(PDO $link){
@@ -17,8 +16,12 @@ class org_clasificacion_dep extends _modelo_parent_sin_codigo{
         $campos_view['codigo'] = array('type' => 'inputs');
         $campos_view['codigo_bis'] = array('type' => 'inputs');
 
+        $columnas_extra['org_clasificacion_dep_n_departamentos'] = /** @lang sql */
+            "(SELECT COUNT(*) FROM org_departamento WHERE org_departamento.org_clasificacion_dep_id = org_clasificacion_dep.id)";
+
         parent::__construct(link: $link, tabla: $tabla, campos_obligatorios: $campos_obligatorios, columnas: $columnas,
-            campos_view: $campos_view, no_duplicados: $no_duplicados, tipo_campos: $tipo_campos);
+            campos_view: $campos_view, columnas_extra: $columnas_extra, no_duplicados: $no_duplicados,
+            tipo_campos: $tipo_campos);
         $this->NAMESPACE = __NAMESPACE__;
     }
 
