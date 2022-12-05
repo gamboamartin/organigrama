2a0036ad13bea6eabbb7157b8547eccad4fe2aa6
Ivan Avelar <ivan_avelar6@hotmail.com>
2022-12-05 12:10:44 -0600

    v0.358.47 Se eliminan botones de DP en alta y modifica empresa

--- templates/inputs/org_empresa/modifica.php
+++ templates/inputs/org_empresa/modifica.php
@@ -2,4 +2,3 @@
 
 include 'templates/inputs/_base/org_empresa/form.php' ?>
 <?php include (new views())->ruta_templates.'botons/submit/modifica_bd.php';?>
-<?php include 'templates/inputs/_base/org_empresa/buttons.php';?>
