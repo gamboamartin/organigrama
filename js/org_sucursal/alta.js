31eb068d5c4f8680844b80a8abb53c4c160f792f
[user] <angel.martinez@ivitec.com.mx>
2022-12-26 16:21:31 -0600

    v1.4.5 Fix js sucursal

--- js/org_sucursal/alta.js
+++ js/org_sucursal/alta.js
@@ -1,6 +1,6 @@
 let url = getAbsolutePath();
 
-document.write('<script src="/../organigrama/js/direcciones.js"></script>');
+document.write(`<script src="${url}/js/direcciones.js"></script>`)
 
 let session_id = getParameterByName('session_id');
 
