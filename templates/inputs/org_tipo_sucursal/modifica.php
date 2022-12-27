eec99f80acf1cb6d25e346366c6703b5b8c8a7bc
mgamboa <gamboa.martin@gmail.com>
2022-12-27 13:42:07 -0600

    v1.5.6 Se integra buttons de acciones a upd

--- templates/inputs/org_tipo_sucursal/modifica.php
+++ templates/inputs/org_tipo_sucursal/modifica.php
@@ -3,3 +3,9 @@
 <?php echo $controlador->inputs->codigo; ?>
 <?php echo $controlador->inputs->descripcion; ?>
 <?php include (new views())->ruta_templates.'botons/submit/modifica_bd.php';?>
+
+<div class="col-row-12">
+    <?php foreach ($controlador->buttons as $button){ ?>
+        <?php echo $button; ?>
+    <?php }?>
+</div>
