c22270c5b776732556be4246d44490baba1a32df
mgamboa <gamboa.martin@gmail.com>
2022-12-29 09:36:11 -0600

    v2.2.1 Se ajusta tipo puesto a new version

--- templates/directivas/org_tipo_puesto_html.php
+++ templates/directivas/org_tipo_puesto_html.php
@@ -8,11 +8,13 @@ use PDO;
 
 
 class org_tipo_puesto_html extends html_controler {
-    public function select_org_tipo_puesto_id(int $cols, bool $con_registros, int $id_selected, PDO $link): array|string
+    public function select_org_tipo_puesto_id(int $cols, bool $con_registros, int $id_selected, PDO $link,
+                                              bool $disabled = false): array|string
     {
         $modelo = new org_tipo_puesto($link);
 
-        $select = $this->select_catalogo(cols:$cols,con_registros:$con_registros,id_selected:$id_selected, modelo: $modelo, label: "Puesto", required: true);
+        $select = $this->select_catalogo(cols: $cols, con_registros: $con_registros, id_selected: $id_selected,
+            modelo: $modelo, disabled: $disabled, label: "Puesto", required: true);
         if(errores::$error){
             return $this->error->error(mensaje: 'Error al generar select', data: $select);
         }
