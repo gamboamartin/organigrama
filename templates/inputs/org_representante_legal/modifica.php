e13133136a0530210597d31b8d38f8bc8f438612
mgamboa <gamboa.martin@gmail.com>
2023-02-21 15:13:37 -0600

    v5.14.7 Se implemneta integracion de parents

--- templates/inputs/org_representante_legal/modifica.php
+++ templates/inputs/org_representante_legal/modifica.php
@@ -7,8 +7,4 @@
 
 <?php include (new views())->ruta_templates.'botons/submit/modifica_bd.php';?>
 
-<div class="col-row-12">
-    <?php foreach ($controlador->buttons as $button){ ?>
-        <?php echo $button; ?>
-    <?php }?>
-</div>
+
