9348836e796a46846d75a6a295f908b799d160e0
mgamboa <gamboa.martin@gmail.com>
2023-02-28 13:51:13 -0600

    v5.24.8 Se implemeta juncionalidad deslecs js upd

--- controllers/controlador_org_empresa.php
+++ controllers/controlador_org_empresa.php
@@ -1037,6 +1037,12 @@ class controlador_org_empresa extends empresas {
 
    public function modifica(bool $header, bool $ws = false): array|stdClass
    {
+       $urls_js = (new _init_dps())->init_js(controler: $this);
+
+       if(errores::$error){
+           return $this->retorno_error(mensaje: 'Error al generar url js',data:  $urls_js,header: $header,ws: $ws);
+       }
+
        $base = $this->base();
        if(errores::$error){
            return $this->retorno_error(mensaje: 'Error al maquetar datos',data:  $base,
