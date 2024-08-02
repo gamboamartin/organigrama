a3c7d63be2dd299e135d5756290b4e14fa28ff2d
mgamboa <gamboa.martin@gmail.com>
2024-08-02 12:12:48 -0600

    v27.5.0 Se integran metodos de instalacion para or

--- instalacion/instalacion.php
+++ instalacion/instalacion.php
@@ -121,6 +121,51 @@ class instalacion
 
     }
 
+    private function _add_org_sucursal(PDO $link): array|stdClass
+    {
+        $out = new stdClass();
+        $create = (new _instalacion(link: $link))->create_table_new(table: 'org_sucursal');
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al create table', data:  $create);
+        }
+        $out->create = $create;
+        $foraneas = array();
+        $foraneas['dp_calle_pertenece_id'] = new stdClass();
+        $foraneas['org_empresa_id'] = new stdClass();
+        $foraneas['org_tipo_sucursal_id'] = new stdClass();
+
+
+        $foraneas_r = (new _instalacion(link:$link))->foraneas(foraneas: $foraneas,table:  'org_sucursal');
+
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al ajustar foranea', data:  $foraneas_r);
+        }
+        $out->foraneas_r = $foraneas_r;
+
+
+        $campos = new stdClass();
+
+        $campos->fecha_inicio_operaciones = new stdClass();
+        $campos->fecha_inicio_operaciones->tipo_dato = 'DATE';
+        $campos->exterior = new stdClass();
+        $campos->interior = new stdClass();
+        $campos->telefono_1 = new stdClass();
+        $campos->telefono_2 = new stdClass();
+        $campos->telefono_3 = new stdClass();
+        $campos->serie = new stdClass();
+
+
+        $result = (new _instalacion(link: $link))->add_columns(campos: $campos,table:  'org_sucursal');
+
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al ajustar campos', data:  $result);
+        }
+
+
+        return $out;
+
+    }
+
     private function _add_org_tipo_empresa(PDO $link): array|stdClass
     {
         $out = new stdClass();
@@ -215,6 +260,17 @@ class instalacion
 
     }
 
+    private function org_sucursal(PDO $link): array|stdClass
+    {
+        $create = $this->_add_org_sucursal(link: $link);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al ajustar create', data:  $create);
+        }
+
+        return $create;
+
+    }
+
     private function org_tipo_empresa(PDO $link): array|stdClass
     {
         $create = $this->_add_org_tipo_empresa(link: $link);
@@ -275,6 +331,11 @@ class instalacion
         $org_empresa = $this->org_empresa(link: $link);
         if(errores::$error){
             return (new errores())->error(mensaje: 'Error al ajustar org_empresa', data:  $org_empresa);
+        }
+
+        $org_sucursal = $this->org_sucursal(link: $link);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al ajustar org_sucursal', data:  $org_sucursal);
         }
 
         $org_departamento = $this->org_departamento(link: $link);
